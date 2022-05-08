0800e8c3756ef36433e0b4050196caaafe8a91f8
Rawson Tabassum <rawson17113@gmail.com>
2022-05-08 20:47:41 +0600

    delete an item completed

--- index.js
+++ index.js
@@ -64,6 +64,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/inventory/:id', async (req, res)=> {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await booksCollection.deleteOne(query);
+            res.send(result);
+        });
+
     } finally {
 
     }
